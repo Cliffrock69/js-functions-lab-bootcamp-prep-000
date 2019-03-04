3e56175a11fe9c1e9c84831a53f1bb3d42e073c3
Cliff Conolly <cliffconolly@protonmail.com>
2019-03-04 20:04:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays () {
   console.log("Happy Holidays!");
 }
+
+happyHolidays()
+
+function happyHolidaysTo (name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+happyHolidaysTo('Nick')
